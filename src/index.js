676a01e883d1b3099ab65f23fa0caf7d98157f1f
dilog <dilog@rambler.ru>
2021-03-05 10:38:35 +0300

    end init project

---
+++ src/index.js
@@ -0,0 +1,17 @@
+//import {Quiz} from '@/components/quiz/Quiz';
+//import {Questions} from '@/components/questions/Questions';
+// import {Answer} from '@/components/answer/Answer';
+// import {Rezult} from '@/components/rezult/Rezult';
+import './scss/index.scss';
+//
+// const quiz = new Quiz('#app', {
+//   components: [Questions, Answer, Rezult],
+// });
+//
+console.log('run');
+
+//const quiz = new Quiz('#app', {
+//  components: [Questions],
+//});
+
+//quiz.render();
